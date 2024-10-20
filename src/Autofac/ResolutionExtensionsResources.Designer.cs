90d70b937f52d068057f8be627bfce151ca99926
HelloKitty <andrew.blakely@ymail.com>
2024-10-19 20:52:03 -0500

    Update ResolutionExtensionsResources.Designer.cs

--- src/Autofac/ResolutionExtensionsResources.Designer.cs
+++ src/Autofac/ResolutionExtensionsResources.Designer.cs
@@ -65,7 +65,7 @@ namespace Autofac {
         /// </summary>
         internal static string CouldNotCastInstance {
             get {
-                return ResourceManager.GetString("CouldNotCastInstance", resourceCulture);
+                return StubbedResourceManager.GetString("CouldNotCastInstance", resourceCulture);
             }
         }
     }
