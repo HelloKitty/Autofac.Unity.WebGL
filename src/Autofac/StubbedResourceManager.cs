a4bf95a7c7c34075c9d821a935e68a61811b24fc
HelloKitty <andrew.blakely@ymail.com>
2024-10-19 20:29:50 -0500

    Create StubbedResourceManager.cs

---
+++ src/Autofac/StubbedResourceManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+
+namespace Autofac
+{
+    /// <summary>
+    /// Stubbed resource manager.
+    /// </summary>
+    public static class StubbedResourceManager
+    {
+        /// <summary>
+        /// Stubs the ResourceManager's GetString function to avoid WebGL compatibility issues.
+        /// </summary>
+        /// <param name="name">Resource string name.</param>
+        /// <param name="culture">Culture.</param>
+        /// <returns>The name of the resource.</returns>
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Usage", "CA1801:Review unused parameters", Justification = "<Pending>")]
+        public static string GetString(string name, CultureInfo culture)
+        {
+            return name;
+        }
+    }
+}
